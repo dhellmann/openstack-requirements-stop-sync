83cbd2900d9204d251389e2c518e909372c97011
Doug Hellmann <doug@doughellmann.com>
2018-03-22 17:23:01 -0400

    go ahead and run lots of local batches
    Signed-off-by: Doug Hellmann <doug@doughellmann.com>

--- local_patches.sh
+++ local_patches.sh
@@ -5,11 +5,7 @@ repo_root=$(dirname $bindir)
 
 source venv/bin/activate
 
-# We could take $* or $@ here but the workflow is really to do one
-# batch at a time so there's not much point and by doing it this way
-# we can use * on the command line so we don't have to keep modifying
-# the command we run.
-batchfiles=$1
+batchfiles=$@
 
 function get_repo_type {
     local repo_dir=$1
