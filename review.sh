01e30d251eef1135b1ea989ec28715e223aaadcb
Doug Hellmann <doug@doughellmann.com>
2018-03-22 12:11:40 -0400

    script to look at patches locally
    Signed-off-by: Doug Hellmann <doug@doughellmann.com>

---
+++ review.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+bindir=$(dirname $(realpath $0))
+repo_root=$(dirname $bindir)
+
+source venv/bin/activate
+
+batchfiles=$@
+
+set -e
+
+for bf in $batchfiles;
+do
+    bf_dir=$repo_root/$(basename $bf)
+    for repo in $(cat $bf);
+    do
+        repo_dir=$bf_dir/$repo
+        git -C $repo_dir show
+    done
+done
