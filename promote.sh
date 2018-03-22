5f9fca654dc4074d14db375d0eaa2974ac05a81b
Doug Hellmann <doug@doughellmann.com>
2018-03-22 17:10:25 -0400

    add script to promote files after working on them
    Signed-off-by: Doug Hellmann <doug@doughellmann.com>

---
+++ promote.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+bindir=$(dirname $(realpath $0))
+repo_root=$(dirname $bindir)
+
+source venv/bin/activate
+
+# We could take $* or $@ here but the workflow is really to do one
+# batch at a time so there's not much point and by doing it this way
+# we can use * on the command line so we don't have to keep modifying
+# the command we run.
+batchfiles=$1
+
+set -e
+
+for bf in $batchfiles;
+do
+    state=$(basename $(dirname $bf))
+    case $state in
+        todo)
+            next=local;;
+        local)
+            next=submitted;;
+        *)
+            echo "Do not know how to promote $bf";
+            exit 1;;
+    esac
+    git -C $bindir mv $bf ${next}/
+    git -C $bindir commit -m "promoted batch $(basename $bf)"
+done
